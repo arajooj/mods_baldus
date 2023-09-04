fb017e26ca43e1fe4ded6c6b3c2a136271c2a8bf
Victor P. Araújo <victor@fdge.com.br>
2023-09-04 02:01:41 -0300

    v0.0.7 - 3

--- gui.py
+++ gui.py
@@ -1,75 +1,111 @@
 import tkinter as tk
-from tkinter import messagebox
+from tkinter import ttk, messagebox
 from pathlib import Path
 import os
 import shutil
 import sys
 import requests
-from tkinter import ttk, messagebox
 from decouple import config
+import logging
+import zipfile
+import py7zr
+import subprocess
 
+# Configuração básica de logging
+logging.basicConfig(filename="modmanager.log", level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
+# ------------------- GitHubReleaseManager -------------------
 class GitHubReleaseManager:
+    """
+    Classe para gerenciar as releases do GitHub.
+    """
     BASE_URL = "https://api.github.com/repos/{owner}/{repo}/releases/latest"
-    
+
     def __init__(self, owner, repo, token=None):
         self.owner = owner
         self.repo = repo
         self.token = token
-    
+
     def get_latest_release(self):
-        headers = {
-            "Accept": "application/vnd.github+json"
-        }
+        headers = {"Accept": "application/vnd.github+json"}
+
         if self.token:
             headers["Authorization"] = f"token {self.token}"
-        
+
         url = self.BASE_URL.format(owner=self.owner, repo=self.repo)
         response = requests.get(url, headers=headers)
-        
+
         if response.status_code == 200:
             return response.json()
         else:
             response.raise_for_status()
 
-
-
+# ------------------- ModManager -------------------
 class ModManager:
-    MOD_FOLDER_REL_PATH = r"Larian Studios\Baldur's Gate 3\Mods"
-    LOCAL_PATH = os.environ['LocalAppData']
-    MOD_FOLDER_ABS_PATH = Path(LOCAL_PATH, MOD_FOLDER_REL_PATH)
-    NATIVE_MODS_PATH = Path("Bin/NativeMods")
-    
+    """
+    Classe para gerenciar os mods do jogo "Baldur's Gate 3".
+    """
+    # Aqui, idealmente, você teria um arquivo de configuração onde essas constantes poderiam ser definidas.
+    # Por simplicidade, estou mantendo-as aqui.
+    # Atualizar o caminho para o diretório _FDGE
+    FDGE_DIR = Path("_FDGE")
+    MOD_FOLDER_REL_PATH = Path("Larian Studios", "Baldur's Gate 3", "Mods")
+    LOCAL_PATH = Path(os.environ['LocalAppData'])
+    MOD_FOLDER_ABS_PATH = LOCAL_PATH / MOD_FOLDER_REL_PATH
+    NATIVE_MODS_PATH = Path("Bin", "NativeMods")
+
     REQUIRED_FOLDERS = ['Bin', 'Data', 'Launcher']
-    FILES_TO_DELETE = ["Bin/bg3.exe", "Bin/bg3_dx11.exe", "Bin/bink2w64.dll"]
-    FOLDERS_TO_DELETE = ["Bin/NativeMods", "Data/Mods", "Data/Public"]
-    
+    FILES_TO_DELETE = [Path("Bin", "bg3.exe"), Path("Bin", "bg3_dx11.exe"), Path("Bin", "bink2w64.dll")]
+    FOLDERS_TO_DELETE = [Path("Bin", "NativeMods"), Path("Data", "Mods"), Path("Data", "Public")]
+
     RENAMES = {
-        "Bin/bink2w64_original.dll": "Bin/bink2w64.dll",
-        "Bin/bg3.exe.backup": "Bin/bg3.exe",
-        "Bin/bg3_dx11.exe.backup": "Bin/bg3_dx11.exe"
+        Path("Bin", "bink2w64_original.dll"): Path("Bin", "bink2w64.dll"),
+        Path("Bin", "bg3.exe.backup"): Path("Bin", "bg3.exe"),
+        Path("Bin", "bg3_dx11.exe.backup"): Path("Bin", "bg3_dx11.exe")
     }
-    
+
     GITHUB_TOKEN = config('GITHUB_TOKEN')
 
     def __init__(self, root):
         self.root = root
-        self.root.title("Gerenciador de Mods - FDGE v7 (Patch #2)")
+        self.setup_gui()
+        logging.info("Inicializando ModManager...")
 
-        icon_path = self.resource_path("_fdge/bard_icon_class_bg3.ico")
+    def setup_gui(self):
+        self.root.title("Gerenciador de Mods - FDGE v7 (Patch #2)")
+        icon_path = self.resource_path("favicon.ico")
         self.root.iconbitmap(icon_path)
 
-        self.lbl_status = tk.Label(root)
+        self.lbl_status = tk.Label(self.root)
         self.lbl_status.pack(pady=20)
 
-        self.btn_activate = tk.Button(root, text="Ativar Mods", command=self.activate_mods)
+        self.update_gui_loading("Verificando atualizações...")
+    
+    def update_gui_loading(self, message):
+        self.lbl_status.config(text=message)
+        self.lbl_status.update()
+        
+    def initialize_app(self):
+        self.FDGE_DIR.mkdir(parents=True, exist_ok=True)  # Garantir que _FDGE exista
+        current_version = self.get_current_version()
+        latest_version = self.get_latest_version()
+        
+        if current_version != latest_version:
+            self.update_gui_loading("Baixando a versão mais recente dos mods...")
+            # Use a função para baixar a nova versão e descompactar para a pasta FDGE
+            self.download_and_extract_latest_mod()
+            with open(self.resource_path("_FDGE/version.txt"), 'w') as f:
+                f.write(latest_version)
+
+        self.show_buttons()
+        
+        
+    def show_buttons(self):
+        self.btn_activate = tk.Button(self.root, text="Ativar Mods", command=self.activate_mods)
         self.btn_activate.pack(pady=10)
 
-        self.btn_deactivate = tk.Button(root, text="Desativar Mods", command=self.deactivate_mods)
+        self.btn_deactivate = tk.Button(self.root, text="Desativar Mods", command=self.deactivate_mods)
         self.btn_deactivate.pack(pady=10)
-        
-        self.btn_test_download = tk.Button(root, text="Testar Download", command=self.test_download)
-        self.btn_test_download.pack(pady=10)
 
         self.check_mods_status()
 
@@ -99,32 +135,7 @@ class ModManager:
 
         self.progress_label.destroy()
         self.progress_bar.destroy()
-        
-    def test_download(self):
-        try:
-            # Defina o token, o proprietário e o nome do repositório aqui
-            token = self.GITHUB_TOKEN
-            owner = "arajooj"
-            repo = "mods_baldus"
-            
-            gh_manager = GitHubReleaseManager(owner, repo, token)
-            release_data = gh_manager.get_latest_release()
-            
-            # Aqui, estou supondo que você deseja pegar o URL de download do primeiro ativo.
-            download_url = release_data['assets'][0]['browser_download_url']
-            
-            # Vamos simplesmente exibir o URL por agora. Você pode integrar um download real aqui.
-            messagebox.showinfo("URL de Download", download_url)
-            
-            file_name = download_url.split('/')[-1]
-            destination_path = Path.cwd() / file_name
 
-            self.download_file_with_progress(download_url, destination_path)
-            
-            messagebox.showinfo("Download Completo", f"Arquivo baixado como {file_name}")
-            
-        except Exception as e:
-            messagebox.showerror("Erro", f"Ocorreu um erro ao obter o URL de download. {e}")
             
     def check_mods_status(self):
         if self.MOD_FOLDER_ABS_PATH.exists() and self.NATIVE_MODS_PATH.exists():
@@ -151,12 +162,11 @@ class ModManager:
         if not self.check_required_folders():
             messagebox.showerror("Erro", "A pasta atual não tem as pastas necessárias (Bin, Data, Launcher).")
             return
-        try:
-            shutil.copytree(self.resource_path("_fdge/_ROOT"), "./", dirs_exist_ok=True)
-            shutil.copytree(self.resource_path("_fdge/_BKP"), "Bin", dirs_exist_ok=True)
 
-            self.MOD_FOLDER_ABS_PATH.mkdir(parents=True, exist_ok=True)
-            shutil.copytree(self.resource_path("_fdge/_PAK"), str(self.MOD_FOLDER_ABS_PATH), dirs_exist_ok=True)
+        try:
+            self._copy_mod_resources()
+            self._copy_backup_resources()
+            self._create_mods_folder()
             
             messagebox.showinfo("Sucesso", "Mods ativados com sucesso!")
         except Exception as e:
@@ -166,30 +176,137 @@ class ModManager:
 
     def deactivate_mods(self):
         try:
-            for f in self.FILES_TO_DELETE:
-                path = Path(f)
-                if path.exists():
-                    path.unlink()
-            for d in self.FOLDERS_TO_DELETE:
-                path = Path(d)
-                if path.exists():
-                    shutil.rmtree(path)
-            if self.MOD_FOLDER_ABS_PATH.exists():
-                shutil.rmtree(self.MOD_FOLDER_ABS_PATH)
-            if Path("_fdge/_BKP").exists():
-                shutil.copytree("_fdge/_BKP", "Bin", dirs_exist_ok=True)
-            for old, new in self.RENAMES.items():
-                old_path, new_path = Path(old), Path(new)
-                if old_path.exists():
-                    old_path.rename(new_path)
+            self._delete_files()
+            self._delete_folders()
+            self._restore_backup_resources()
             
             messagebox.showinfo("Sucesso", "Mods desativados com sucesso!")
         except Exception as e:
             messagebox.showerror("Erro", f"Ocorreu um erro ao desativar os mods. {e}")
 
         self.check_mods_status()
+        
+    # Métodos auxiliares para refatoração:
+    def _copy_mod_resources(self):
+        shutil.copytree(self.resource_path("_fdge/_ROOT"), "./", dirs_exist_ok=True)
 
-    def center_window(self, width=400, height=300):
+    def _copy_backup_resources(self):
+        shutil.copytree(self.resource_path("_fdge/_BKP"), "Bin", dirs_exist_ok=True)
+
+    def _create_mods_folder(self):
+        self.MOD_FOLDER_ABS_PATH.mkdir(parents=True, exist_ok=True)
+        shutil.copytree(self.resource_path("_fdge/_PAK"), str(self.MOD_FOLDER_ABS_PATH), dirs_exist_ok=True)
+
+    def _delete_files(self):
+        for f in self.FILES_TO_DELETE:
+            path = Path(f)
+            if path.exists():
+                path.unlink()
+
+    def _delete_folders(self):
+        for d in self.FOLDERS_TO_DELETE:
+            path = Path(d)
+            if path.exists():
+                shutil.rmtree(path)
+        if self.MOD_FOLDER_ABS_PATH.exists():
+            shutil.rmtree(self.MOD_FOLDER_ABS_PATH)
+
+    def _restore_backup_resources(self):
+        if Path("_fdge/_BKP").exists():
+            shutil.copytree("_fdge/_BKP", "Bin", dirs_exist_ok=True)
+        for old, new in self.RENAMES.items():
+            old_path, new_path = Path(old), Path(new)
+            if old_path.exists():
+                old_path.rename(new_path)
+                
+    def get_current_version(self):
+        version_file = self.FDGE_DIR / "version.txt"
+        if version_file.exists():
+            with open(version_file, 'r') as f:
+                return f.read().strip()
+        return None
+
+    def get_latest_version(self):
+        try:
+            # Aqui, você está usando a classe já definida GitHubReleaseManager
+            token = self.GITHUB_TOKEN
+            owner = "arajooj"
+            repo = "mods_baldus"
+            gh_manager = GitHubReleaseManager(owner, repo, token)
+            release_data = gh_manager.get_latest_release()
+            return release_data['tag_name']
+        except Exception as e:
+            logging.error(f"Erro ao tentar obter a versão mais recente: {e}")
+            return None
+        
+        
+    def download_and_extract_latest_mod(self):
+        try:
+            token = self.GITHUB_TOKEN
+            owner = "arajooj"
+            repo = "mods_baldus"
+
+            gh_manager = GitHubReleaseManager(owner, repo, token)
+            release_data = gh_manager.get_latest_release()
+
+            tag_name = release_data['tag_name']
+            version_file_path = self.FDGE_DIR / "version.txt"
+
+            if version_file_path.exists() and version_file_path.read_text().strip() == tag_name:
+                logging.info(f"Versão {tag_name} já baixada. Pulando o download.")
+                return
+
+            download_url = f"https://github.com/{owner}/{repo}/releases/download/{tag_name}/{tag_name}.zip"
+            destination_path = self.FDGE_DIR / f"{tag_name}.zip"
+
+            self.download_file_with_progress(download_url, destination_path)
+
+
+            # Setup extraction progress UI
+            self.progress_label = tk.Label(self.root, text="Extraindo arquivos...")
+            self.progress_label.pack(pady=5)
+
+            self.progress_bar = ttk.Progressbar(self.root, orient='horizontal', length=300, mode='determinate')
+            self.progress_bar.pack(pady=10)
+
+            # Extract the ZIP archive
+            with zipfile.ZipFile(destination_path, 'r') as zip_ref:
+                total_files = len(zip_ref.infolist())
+                extracted_files = 0
+                for file_info in zip_ref.infolist():
+                    extracted_files += 1
+                    extraction_progress = (extracted_files / total_files) * 100
+                    self.update_extraction_progress(extraction_progress)
+
+                    zip_ref.extract(file_info, self.FDGE_DIR)
+
+            os.remove(destination_path)
+            
+            if hasattr(self, 'progress_label'):
+                self.progress_label.destroy()
+            if hasattr(self, 'progress_bar'):
+                self.progress_bar.destroy()
+
+
+            # Escrever a versão no arquivo apenas se o download e a extração forem bem-sucedidos
+            with open(version_file_path, "w") as version_file:
+                version_file.write(tag_name)
+
+        except Exception as e:
+            logging.error(f"Erro ao tentar download_and_extract_latest_mod: {e}")
+            messagebox.showerror("Erro", f"Ocorreu um erro ao baixar e extrair o mod. {e}")
+
+
+
+
+    def update_extraction_progress(self, progress):
+        if hasattr(self, 'progress_label'):
+            self.progress_label.config(text=f"Extraindo arquivos: {progress:.2f}%")
+        if hasattr(self, 'progress_bar'):
+            self.progress_bar.config(value=progress)
+        self.root.update()
+    
+    def center_window(self, width=400, height=200):
         screen_width = self.root.winfo_screenwidth()
         screen_height = self.root.winfo_screenheight()
         x = (screen_width / 2) - (width / 2)
@@ -197,14 +314,21 @@ class ModManager:
         self.root.geometry('%dx%d+%d+%d' % (width, height, x, y))
 
 if __name__ == "__main__":
-    root = tk.Tk()
-    root.geometry("400x300")
-    root.resizable(False, False)
+    try:
+        root = tk.Tk()
+        root.geometry("400x200")
+        root.resizable(False, False)
 
-    app = ModManager(root)
-    app.center_window()
+        app = ModManager(root)
+        
+        app.center_window()  # Centraliza a janela antes de inicializar
+        app.initialize_app()
 
-    root.mainloop()
+        root.mainloop()
+
+    except Exception as e:
+        logging.critical(f"Erro crítico ao executar o aplicativo: {e}")
+
 
 # Build command (Windows): 
 # python -m PyInstaller --onefile --windowed --icon=favicon.ico --add-data="_fdge;_fdge" --distpath=out/dist --workpath=out/build --name="bg3_fdge_modmanager_v7" --log-level=DEBUG --clean gui.py
